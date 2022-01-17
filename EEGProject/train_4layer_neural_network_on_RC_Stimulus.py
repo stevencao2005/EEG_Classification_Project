d99d13455cf40cd075354e98d45f133a8ebb4917
Steven Cao <stevenCao05@gmail.com>
2022-01-17 10:54:48 -0800

    Added another part of the directions to say whether you want to use the data before subject removal or the data after subject removal and made the case-by-case analysis function, fit_classifier, and fitted_classifier compatible with both ways

--- EEGProject/train_4layer_neural_network_on_RC_Stimulus.py
+++ EEGProject/train_4layer_neural_network_on_RC_Stimulus.py
@@ -47,9 +47,10 @@ from four_layer_neural_network import Net
 
 def case_by_case_analysis(y_true, y_pred):
     correctPredictionIndexes = []
+    labels                   = np.unique(y_true)
     indexes                  = np.arange(1, len(y_true) + 1)
-    subjectNames             = [i for i in range(21)]
-    subjectNames_str         = [str(i) for i in range(21)]
+    subjectNames             = [i for i in labels]
+    subjectNames_str         = [str(i) for i in labels]
 
     #GETTING THE CONFUSION MATRIX
     confusionMX              = confusion_matrix(y_true, y_pred)
@@ -62,7 +63,7 @@ def case_by_case_analysis(y_true, y_pred):
     #GETTING THE AMOUNT OF SAMPLES PREDICTED CORRECTLY FOR EACH SUBJECT
     print("amount of samples predicted correctly for each subject")
     for i in range(confusionMX.shape[0]):
-        print('    subject' + str(i), confusionMX[i, i])
+        print('    subject' + str(subjectNames[i]), confusionMX[i, i])
 
     #GETTING THE PREDICTIONS FOR EACH SAMPLE AND GROUP THEM ON WHETHER IT WAS CORRECT OR INCORRECT
     predictions = dict()
@@ -122,13 +123,14 @@ def case_by_case_analysis(y_true, y_pred):
     return predictions
 
 def fit_classifier():
+
     #LOAD AND PREPROCESS THE DATASET
     LoaderPreprocessor               = Load_And_Preprocess_Dataset()
     BED_dataset                      = LoaderPreprocessor.func_data_load_SPEC_RC_preprocessed()
-    X_train, X_test, Y_train, Y_test = LoaderPreprocessor.func_dataPreProcessing(BED_dataset, categorical='true')
+    X_train, X_test, Y_train, Y_test = LoaderPreprocessor.func_dataPreProcessing(BED_dataset, toCategorical='true', subjectRemoval=sys.argv[2])
 
     #IF PYTORCH, CREATE TENSOR DATASET
-    train_loader, test_loader = LoaderPreprocessor.func_createTensorDataset(X_train, X_test, Y_train, Y_test)
+    train_loader, test_loader        = LoaderPreprocessor.func_createTensorDataset(X_train, X_test, Y_train, Y_test)
 
     #4-LAYERED NEURAL NETWORK USING PYTORCH
     model     = Net()
@@ -136,7 +138,7 @@ def fit_classifier():
     criterion = nn.CrossEntropyLoss()
 
     #TRAINING THE MODEL
-    time1 = time.time()
+    time1    = time.time()
     model.train(train_loader, optimizer, criterion)
     duration = time.time() - time1
     print("time took to train the model", duration)
@@ -154,10 +156,11 @@ def fit_classifier():
     torch.save(model.state_dict(), file_name)
 
 def fitted_classifier():
+
     #LOAD AND PREPROCESS THE DATASET
     LoaderPreprocessor               = Load_And_Preprocess_Dataset()
     BED_dataset                      = LoaderPreprocessor.func_data_load_SPEC_RC_preprocessed()
-    X_train, X_test, Y_train, Y_test = LoaderPreprocessor.func_dataPreProcessing(BED_dataset, categorical='true')
+    X_train, X_test, Y_train, Y_test = LoaderPreprocessor.func_dataPreProcessing(BED_dataset, toCategorical='true', subjectRemoval=sys.argv[2])
 
     #IF PYTORCH, CREATE TENSOR DATASET
     train_loader, test_loader        = LoaderPreprocessor.func_createTensorDataset(X_train, X_test, Y_train, Y_test)
@@ -168,17 +171,19 @@ def fitted_classifier():
     criterion        = nn.CrossEntropyLoss()
 
     #LOAD THE ALREADY FITTED MODEL
-    file_name = os.path.abspath('.') + '/saved_datasets/RC_model_weights_NN.pth'
+    file_name        = os.path.abspath('.') + '/saved_datasets/RC_model_weights_NN.pth'
     model_loaded.load_state_dict(torch.load(file_name))
 
     #EVALUATING THE MODEL
     y_pred, y_true, metrics, confusionMatrix = model_loaded.compute_performance_metrics(test_loader)
+    predictions                              = case_by_case_analysis(y_true, y_pred)
+
     print("accuracy:  ", metrics['accuracy'])
     print("precision: ", metrics['precision'])
     print("recall:    ", metrics['recall'])
     print("f1_score:  ", metrics['f1 score'])
 
-    predictions = case_by_case_analysis(y_true, y_pred)
+
 
     i=1
 
@@ -188,12 +193,13 @@ def main():
 if __name__ == '__main__':
     #STARTING CODE --------
 
-    # DIRECTIONS ON WHAT TO DO
-    sys.argv.extend(['load'])
+    # DIRECTIONS ON WHAT TO DO AND WHETHER WE WANT TO USE THE DATA BEFORE OR AFTER SUBJECT REMOVAL
+    sys.argv.extend(['load', 'before subject removal'])
 
     if sys.argv[1] == 'train':
         #TRAINS A 4-LAYER NEURAL NETWORK ON THE PREPROCESSED DATA
         fit_classifier()
+
     elif sys.argv[1] == 'load':
         #LOADS THE TRAINED 4-LAYER NEURAL NETWORK
         fitted_classifier()
